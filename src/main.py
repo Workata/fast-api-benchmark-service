db44439db875775294ef8c2b7aaf8728ab777848
workata <jakubtolsciuk@gmail.com>
2023-03-27 11:12:22 +0000

    Fix stuff

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 from typing import Dict
 
-from fastapi import FastAPI
+
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import models #crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
